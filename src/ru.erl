9f05c4293738e14bdcc1b904c56ad08ccd1754d1
jz <jeff.zellner@gmail.com>
2010-11-01 18:40:41 -0400

    smarter dog\! (not much)

--- src/ru.erl
+++ src/ru.erl
@@ -136,6 +136,13 @@ random_direction() ->
     Dirs = [kp_n, kp_s, kp_e, kp_w, kp_sw, kp_nw, kp_se, kp_ne],
     lists:nth(random:uniform(length(Dirs)), Dirs).
 
+random_direction(Not) ->
+    random:seed(now()),
+    Dirs = lists:filter(fun(Elem) -> Elem =/= Not end,
+        [kp_n, kp_s, kp_e, kp_w, kp_sw, kp_nw, kp_se, kp_ne]),
+    lists:nth(random:uniform(length(Dirs)), Dirs).
+
+
 distance_between({X1, Y1}, {X2, Y2}) ->
     math:sqrt(math:pow(X2-X1, 2) + math:pow(Y2-Y1, 2)).
 
@@ -148,6 +155,7 @@ dog_brain(Event, Me) ->
             {HX,HY} = HeroLoc#world.loc,
             Distance = distance_between({CX,CY}, {HX,HY}),
             if
+                %% if hero is more than 3 away, move towards
                 Distance >= 3 ->
                     DX = case HX - CX of
                         0 -> 0; A when A >= 1 -> 1; A when A =< -1 -> -1
@@ -157,13 +165,20 @@ dog_brain(Event, Me) ->
                     end,
                     Dir = ru_util:coordinate_delta_direction({DX,DY});
 
+                %% meander if close to hero
                 Distance < 3 ->
                     Dir = random_direction()
             end,
-            case ru_state:move(Me, Dir) of
-                error -> 
-                    ru_state:move(Me, random_direction());
-                ok -> ok
+
+            %% dont move on top of hero's square
+            case ru_util:direction_coords({CX,CY}, Dir) of
+                {HX, HY} -> ru_state:move(Me, random_direction(Dir));
+                _ ->
+                    case ru_state:move(Me, Dir) of
+                        error -> 
+                            ru_state:move(Me, random_direction());
+                        ok -> ok
+                    end
             end;
 
         _ -> ok
